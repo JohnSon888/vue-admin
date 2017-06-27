c021896b08388effc0d8df88bdc63c9b3b0ee394
lixiang <549573735@qq.com>
2017-06-27 16:30:51 +0800

    333

---
+++ src/components/navListHover/moneyHover.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="box">
+		<div class="box-main">money</div>
+	</div>
+</template>
+<script type="text/javascript">
+	
+</script>
+<style type="text/css">
+	.box{position: relative;display: inline;}
+	.box-main{position: absolute;width:auto;height:auto;background:#fff;top: -13px;left: 46px;color:#333;z-index:9;box-shadow:1px 1px 3px #888888;}
+</style>
---
+++ src/components/navListHover/showbox.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="showbox" :class="{'showb':flag}">
+		<orderHover v-if='flag1&&flag'></orderHover>
+		<goodsHover v-if='flag2&&flag'></goodsHover>
+		<clientHover v-if='flag3&&flag'></clientHover>
+		<moneyHover v-if='flag4&&flag'></moneyHover>
+		<marketingHover v-if='flag5&&flag'></marketingHover>
+		<statementHover v-if='flag6&&flag'></statementHover>
+	</div>
+</template>
+<script type="text/javascript">
+
+import orderHover from './orderHover'
+import goodsHover from './goodsHover'
+import clientHover from './clientHover'
+import moneyHover from './moneyHover.vue'
+import marketingHover from './marketingHover'
+import statementHover from './statementHover'
+	export default {
+		props:['flag','index'],
+		data(){
+			return{
+	            flag1:false,
+	            flag2:false,
+	            flag3:false,
+	            flag4:false,
+	            flag5:false,
+	            flag6:false,
+			}
+		},
+		created(){
+			var num = this.index+1
+            this['flag'+num]=true;
+		},
+		components:{
+	        orderHover,
+	        goodsHover,
+	        clientHover,
+	        moneyHover,
+	        marketingHover,
+	        statementHover,
+		}
+
+	}
+</script>
+<style type="text/css" scoped>
+	.showb {
+		display: none;
+	}
+	.showbox {display: inline;}
+</style>
